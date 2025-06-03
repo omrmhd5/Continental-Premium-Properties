e1930ee86d2a85c73d28b6ca04d1b84ad7ac6da1
omrmhd5 <omrmhd54@gmail.com>
2025-06-03 03:54:55 +0300

    Added Cloudinary

---
+++ Back-End/utils/cloudinary.js
@@ -0,0 +1,9 @@
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: "dllmcgx5k",
+  api_key: "179813583724671",
+  api_secret: "yhXo-T0Ak4eZgeP7eZ1RXD5C2LE",
+});
+
+module.exports = cloudinary;
